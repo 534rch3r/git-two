e7b52d437bcefb895cc4a6a367ee26369a0956bb
hf <hhhhhhhffffffffff@gmail.com>
2020-09-01 22:10:12 +0300

    removed grids in order to use .pack() function at the end(because of prblems using both at the same time) also added button

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import tkinter as tk
 
 window = tk.Tk()
-#root = Tk()
+#tutorial: root = Tk()
 
-label1 = tk.Label(text="label1").grid(row=0, column=0)
-label2 = tk.Label(text="label2").grid(row=1, column=5)
-label3 = tk.Label(text="label3").grid(row=2, column=3)
-#label = Label(root, text="Hello world")
+def button1_click():
+    label_button1 = tk.Label(text="label_button1")
+    label_button1.pack()
 
 
+#label1 = tk.Label(text="label1").grid(row=0, column=0)
+#label2 = tk.Label(text="label2").grid(row=1, column=5)
+#label3 = tk.Label(text="label3").grid(row=2, column=3)
+#tutorial: label = Label(root, text="Hello world")
 #label1.grid(row=0, column=0)
 #label2.grid(row=1, column=5)
 #label3.grid(row=2, column=3)
 
+label1 = tk.Label(text="label1")
+label2 = tk.Label(text="label2")
+label3 = tk.Label(text="label3")
 
-#label1.pack()
-#label2.pack()
-#label.pack()
+button1 = tk.Button(text="button1", 
+#state=tk.DISABLED,
+command=button1_click,padx=50, pady=50, fg="blue", bg="red")
+#command=button1_click,padx=50, pady=50).grid(row=3, column=4)
+#tutorial: button1 = Button(root,text="button1", state=DISABLED)
 
-
+button1.pack()
+label1.pack()
+label2.pack()
+label3.pack()
 
 '''
 window.mainloop() tells Python to run the Tkinter event loop.
@@ -28,4 +39,4 @@ called on is closed. Go ahead and close the window you’ve created,
 and you’ll see a new prompt displayed in the shell.
 '''
 window.mainloop()
-#root.mainloop()
+#tutorial: root.mainloop()
